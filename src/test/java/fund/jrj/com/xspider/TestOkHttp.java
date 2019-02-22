8de331410661d959374444336f35415586734b83
huangyan <huangyan@huangyan-PC>
2019-02-23 00:14:58 +0800

    测试okhttp

--- src/test/java/fund/jrj/com/xspider/TestOkHttp.java
+++ src/test/java/fund/jrj/com/xspider/TestOkHttp.java
@@ -1,10 +1,32 @@
 package fund.jrj.com.xspider;
 
+import java.io.IOException;
+
+import com.alibaba.fastjson.JSON;
+
+import fund.jrj.com.xspider.utils.OkhttpUtils;
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.Response;
+
 public class TestOkHttp {
 
 	public static void main(String[] args) {
-		
+		String url="http://fund.jrj.com.cn";
+		OkhttpUtils.getInstance().doGet(url, new Callback() {
+			@Override
+			public void onFailure(Call call, IOException e) {
+			}
 
+			@Override
+			public void onResponse(Call call, Response response) throws IOException {
+				System.out.println(response.code());
+				System.out.println(response.body().string());
+				System.out.println(JSON.toJSONString(response));
+			}
+
+		});
+		
 	}
 
 }
