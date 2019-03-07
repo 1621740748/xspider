39feb417fe428c7580d9445d1342cced24e965ed
huangyan <yan.huang@jrj.com.cn>
2019-03-07 11:29:31 +0800

    use webkit

--- br.py
+++ br.py
@@ -4,43 +4,36 @@ from PySide.QtGui import QApplication
 from PySide.QtGui import QMainWindow
 from PySide.QtWebKit import QWebView, QWebPage
 from PySide.QtNetwork import QNetworkAccessManager
-
-class Browser(QMainWindow):
-    def __init__(self, parent= None):
-        super(Browser, self).__init__( parent)
+import threading
+class Browser:
+    def __init__(self, app):
         self.network_manager = QNetworkAccessManager()
         self.network_manager.createRequest = self._create_request
-
         self.web_page = QWebPage()
         self.web_page.setNetworkAccessManager(self.network_manager)
-
         self.web_view = QWebView()
         self.web_view.setPage(self.web_page)
-
         self.html_data = None
-
+        self.app =app
+        self.finished=threading.Event()
+        self.resultList=[]
     def _create_request(self, operation, request, data):
         reply = QNetworkAccessManager.createRequest(self.network_manager,
                                                     operation,
                                                     request,
                                                     data)
-        print request.url().toString()
+        self.resultList.append(request.url().toString())
         return reply
 
-    def slotSourceDownloaded(self):
-         reply=self.sender();
-         a=QTextStream(reply).readAll()
-         print a
-         reply.deleteLater()
-def myLoadFinish():
-    print "finished"
-    sys.exit(0)
-
+    def myLoadFinish(self):
+        app.quit()
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    browser = Browser()
-    browser.web_view.load("http://fund.jrj.com.cn");
-    browser.web_view.loadFinished.connect(myLoadFinish);
+    url=sys.argv[1]
+    browser = Browser(app)
+    browser.web_view.load(url);
+    browser.web_view.loadFinished.connect(browser.myLoadFinish);
     app.exec_()
+    print ('\n'.join(browser.resultList[1:]))
 
--- br2.py
+++ br2.py
@@ -1,17 +1,20 @@
 import os
-result = os.popen('python br.py')
-print 30*'*'
-res = result.read()
-for line in res.splitlines():
-    print line
-    print 20*'^'
-print 30*'-------'
+def getAutoLoadUrl(url):
+    resultList=[]
+    result = os.popen('python br.py '+url)
+    res = result.read()
+    for line in res.splitlines():
+        if line !=None and line.startswith("http"):
+            resultList.append(line)
+    return resultList
 
-result = os.popen('python br.py')
-print 30*'*'
-res = result.read()
-for line in res.splitlines():
-    print line
-    print 20*'^'
-print 30*'-------'
 
+l=getAutoLoadUrl("http://fund.jrj.com.cn");
+print(20*'--')
+print('\n'.join(l))
+print len(l)
+
+l=getAutoLoadUrl("https://fund.jrj.com.cn");
+print(20*'--')
+print('\n'.join(l))
+print len(l)
