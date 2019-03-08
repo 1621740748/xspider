33386f4ac7127cf27970cadf596912d2083e50ab
huangyan <yan.huang@jrj.com.cn>
2019-03-08 15:58:15 +0800

    数据库

--- spider.sql
+++ spider.sql
@@ -38,3 +38,14 @@ create table  host_certificate(
   private_key varchar(4096) comment '私钥',
   create_time timestamp default CURRENT_TIMESTAMP comment '创建时间'
 );
+create table page_link(
+  link_id int auto_increment primary key comment '链接ID' ,
+  link_url varchar(256) comment '链接',
+  link_host varchar(128) comment '链接host',
+  link_parent_url varchar(256) comment '父链接',
+  page_type tinyint comment '页面类型 1、js 2、css 3、img 4、html',
+  auto_adapt tinyint default 0 comment '是否自适应 0 否 1 是',
+  http_exist tinyint default 0 comment '是否存在http内容',
+  http_exist_content varchar(2048) default '' comment 'http内容',
+  create_time timestamp default CURRENT_TIMESTAMP comment '创建时间'
+);
